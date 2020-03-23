4f1d5410fe7b8add5a95aec78f3e3b27835580b5
caihao.cui@ieee.org <caihao.cui@ieee.org>
2020-03-23 22:52:45 +1100

    update the data_manipulation.py file for data explore.

--- src/data_manipulation.py
+++ src/data_manipulation.py
@@ -1,4 +1,6 @@
 # %% loading packages.
+import itertools
+import numpy
 import turicreate as tc
 
 # %% reload the data.
@@ -27,7 +29,7 @@ songs['year'].show()
 # %% an SFrame with only dated songs.
 # #This basic filter operation
 dated_songs = songs[songs['year'] != None]
-songs['year'].summary()
+dated_songs['year'].summary()
 dated_songs
 
 # %%
@@ -57,4 +59,53 @@ usage_groups = usage_data.groupby(['song_id'], {'total_listens': tc.aggregate.SU
 usage_groups.join(songs, ['song_id']).topk('total_listens')
 
 
+# %% table is already in a great format for feeding to a recommender algorithm
+# as it has user and song identifiers,
+# and some form of a metric (number of
+# listens) to rate how much the user liked
+# the song. The problem here is that users that listened
+# to a song a lot would skew the recommendations.
+
+s = usage_data['listen_count'].summary()
+buckets = numpy.linspace(s.quantile(.005), s.quantile(.995), 5)
+
+
+def bucketize(x):
+    cur_bucket = 0
+    for i in range(0, 5):
+        cur_bucket += 1
+        if x <= buckets[i]:
+            break
+    return cur_bucket
+
+
+usage_data['rating'] = usage_data['listen_count'].apply(bucketize)
+usage_data
+# %% Working with Complex Types
+albums = songs.groupby(['release', 'artist_name'], {'tracks': tc.aggregate.CONCAT('title'),
+                                                    'years': tc.aggregate.CONCAT('year')})
+albums = songs.groupby(['release', 'artist_name', 'year'], {
+                       'tracks': tc.aggregate.CONCAT('title')})
+albums = albums.unstack(column=['year', 'tracks'],
+                        new_column_name='track_dict')
+
+albums['num_years'] = albums['track_dict'].item_length()
+albums['num_years'].show()
+
 # %%
+albums['track_list'] = albums['track_dict'].dict_values()
+albums
+
+# %%
+albums['track_list'] = albums['track_list'].apply(
+    lambda x: list(itertools.chain(*x)))
+
+albums['track_dict'] = albums['track_dict'].dict_trim_by_keys([1994, 1999])
+
+albums[albums['track_dict'].dict_has_any_keys(1965)]
+
+albums
+
+# %%
+big_list = albums.pack_columns(albums.column_names())
+big_list
